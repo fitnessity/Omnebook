1ad1c4e171ad62bf8257391aaa5ba9b8bd41745b
odin <odin@1in.tw>
2023-03-06 01:15:12 -0700

    fix

--- app/Http/Controllers/Personal/OrderController.php
+++ app/Http/Controllers/Personal/OrderController.php
@@ -31,14 +31,18 @@ class OrderController extends PersonalBaseController
 
     public function index(Request $request)
     {
+
         $user = Auth::user();
 
 
         if($request->business_id){
+
             $bookingDetail = [];
             $bookingDetail =  $this->booking_repo->getCurrentUserBookingDetails($request->serviceType, $request->business_id);
             $currentbookingstatus =[];
+
             $currentbookingstatus = $this->booking_repo->getcurrenttabdata($request->serviceType,$request->business_id);
+            
 
             return view('personal.orders.index', [
                 'bookingDetail' => $bookingDetail ,
