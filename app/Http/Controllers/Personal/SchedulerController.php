8b4f0b28b3f3f69e0dd18bf243eee4ab28a0fcf0
Purvi Patel <purvikhoyani14@gmail.com>
2023-08-16 09:59:57 +0000

    Schedule Activity

--- app/Http/Controllers/Personal/SchedulerController.php
+++ app/Http/Controllers/Personal/SchedulerController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers\Personal;
-
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\{UserBookingDetail,BookingCheckinDetails,UserBookingStatus,BusinessActivityScheduler,Customer,SGMailService};
