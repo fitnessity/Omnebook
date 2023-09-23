9ef1fde330cec448f10b85ffb0749d157a9a89a1
Purvi Patel <purvikhoyani14@gmail.com>
2023-09-23 12:05:33 +0000

    sales report and schedular changes

--- resources/views/layouts/business/businesssidebar.blade.php
+++ resources/views/layouts/business/businesssidebar.blade.php
@@ -149,8 +149,8 @@
 					</a>
 					<div class="collapse menu-dropdown" id="sidebarForms">
 						<ul class="nav nav-sm flex-column">
-							<li class="nav-item">
-								<a href="#" class="nav-link" data-key="t-basic-elements">Sales Reports</a>
+							<li class="nav-item @if(Route::current()->getName()=='business.sales_report.index') tab-active @endif">
+								<a @if($companyId)  href="{{route('business.sales_report.index')}}" @endif class="nav-link" data-key="t-basic-elements">Sales Reports</a>
 							</li>
 							<li class="nav-item @if(Route::current()->getName()=='business.member_expirations.index') tab-active @endif">
 								<a @if($companyId)  href="{{route('business.member_expirations.index')}}" @endif class="nav-link" data-key="t-form-select">Membership Expirations</a>
