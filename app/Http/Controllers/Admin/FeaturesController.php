019d8d183450b3a05fb2cff6449f72191d7c6a71
Purvi Patel <purvikhoyani14@gmail.com>
2023-11-10 13:11:32 +0000

    Plan and mail

---
+++ app/Http/Controllers/Admin/FeaturesController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Repositories\{FeaturesRepository};
+use Auth;
+
+class FeaturesController extends Controller
+{  
+
+	protected $features;
+
+	public function __construct(FeaturesRepository $features)
+    {
+        $this->middleware('admin');
+        $this->features = $features;
+    }
+
+    public function index()
+    {
+        $features = $this->features->getAllFeatures();
+
+        return view('admin.features.index', [
+            'features' => $features,
+            'pageTitle' => 'Manage Features'
+        ]);
+    }
+
+    public function edit($id)
+    {
+        $features = $this->features->getById($id);
+        if($features){
+            return view('admin.features.edit', [
+                'pageTitle' => 'Edit Features',
+                'features' => $features
+            ]);
+        }
+
+        return redirect()->route('features.index');   
+    }
+}
