f8e8051b16b3fe42e5176f19fde3be3decc920fe
Purvi Patel <purvikhoyani14@gmail.com>
2023-11-27 15:23:18 +0000

    promocode and plan features  admin side

--- resources/views/admin/promo_codes/index.blade.php
+++ resources/views/admin/promo_codes/index.blade.php
@@ -2,30 +2,35 @@
 @section('content')
 
     <div id="systemMessage"></div>
-
+    <p>
+        <a href="{{route('promo_codes.create')}}" class="btn btn-success">Add New</a>
+    </p>
     <div class="panel panel-default">
         <div class="panel-heading">List</div>
         <div class="panel-body">
-
-       
             <div class="table-responsive">
-              <table id="plan_list" class="table table-bordered table-striped {{ count($features) > 0 ? 'datatable' : '' }} table-hover ">
+              <table id="promo_codes_list" class="table table-bordered table-striped {{ count($promoCodes) > 0 ? 'datatable' : '' }} table-hover ">
                 <thead>
                     <tr>
                       <th>Title</th>
-                      <th>Tooltip Information</th>
+                      <th>Code</th>
+                      <th>Discount</th>
+                      <th>Status</th>
                       <th>Action</th>
                     </tr>
                 </thead>
 
                 <tbody>
-                    @if (count($features) > 0)
-                        @foreach ($features as $i=>$value)
+                    @if (count($promoCodes) > 0)
+                        @foreach($promoCodes as $i=>$value)
                           <tr id="item-{!! $value->id !!}">
-                            <td><a href="\admin\features\edit\{{$value->id}}" title="Click to edit {{@$value->title}}">{{@$value->name}}</a></td>
-                            <td>{{ $value->tooltip_text}}</td>
+                            <td><a href="\admin\promo_codes\edit\{{$value->id}}" title="Click to edit {{@$value->title}}">{{@$value->title}}</a></td>
+                            <td>{{ $value->code}}</td>
+                            <td>{{ $value->price_in == '$' ? '$'.@$value->price : @$value->price.'%' }}</td>
+                            <td>{{ $value->status}}</td>
                             <td>
-                              <a href="\admin\features\edit\{{$value->id}}" title="Click to edit {{@$value->title}}" class="btn btn-xs btn-primary"><i class="fa fa-pencil" aria-hidden="true"></i></a>
+                              <a href="\admin\promo_codes\edit\{{$value->id}}" title="Click to edit {{@$value->title}}" class="btn btn-xs btn-primary"><i class="fa fa-pencil" aria-hidden="true"></i></a>
+                              <a onclick="confirmDelete({{ $value->id }})" title="Click to delete {{@$value->title}}" class="btn btn-xs btn-primary"><i class="fa fa-trash" aria-hidden="true"></i></a>
                             </td>
                           </tr>
                         @endforeach
@@ -42,14 +47,14 @@
 
   <input type="hidden" id="ajaxToken" name="_token" value="{{ csrf_token() }}">
 
-<?php if(count($features)){?>
+<?php if(count($promoCodes)){?>
 
 <script>
 
   $(function () {
 
      var oTable;
-      oTable = $('#plan_list').DataTable({
+      oTable = $('#promo_codes_list').DataTable({
       "paging": true,
       "lengthChange": true,
       "searching": true,
@@ -73,17 +78,15 @@
     $.fn.dataTable.ext.search.push(
     function( settings, data, dataIndex ) 
     {
-        var status              = $('#filter-select').val(),
-       status_clm          =  data[5];
-         if(status == 'Yes') {
-            if (status_clm == 'Yes')
-            {
+        var status = $('#filter-select').val(),
+        status_clm =  data[5];
+        if(status == 'Yes') {
+            if (status_clm == 'Yes'){
                 return true;
             }
             return false;
-        } else if(status == 'No') {
-          if (status_clm == 'No')
-            {
+        }else if(status == 'No') {
+            if (status_clm == 'No'){
                 return true;
             }
             return false;
@@ -99,15 +102,11 @@
 
   });
 
-
-
-  var moduleConfig = {
-        deletePlanUrl: '{!! route('delete-plan') !!}',
-        deactivatePlanUrl: '{!! route('deactivate-plan') !!}',
-        activatePlanUrl: '{!! route('activate-plan') !!}'
-  }
-
-
+    function confirmDelete(id) {
+        if (confirm("Are you sure you want to delete this Promo Code?")) {
+            window.location.href = "/admin/promo_codes/delete/" + id;
+        }
+    }
 
 </script>
 
