b2c40dce77a18d0887e75aca250ab2aa8030e216
odin <odin@1in.tw>
2023-11-27 06:45:48 -0500

    fix show status, fix suspend and other bugs

--- app/Http/Controllers/Business/UserBookingDetailController.php
+++ app/Http/Controllers/Business/UserBookingDetailController.php
@@ -86,7 +86,7 @@ class UserBookingDetailController extends Controller
             }
 
             $result = [
-                'status' => 'cancel',
+                'status' => 'refund',
                 'expired_at' => date('Y-m-d',strtotime($request->refund_date)),
                 "refund_amount" => $request->refund_amount,
                 "refund_method" => $request->refund_method,
@@ -123,9 +123,26 @@ class UserBookingDetailController extends Controller
     public function suspend(Request $request, $business_id){
         $company = $request->current_company->findOrFail($business_id);
         $customer = $company->customers()->findOrFail($request->customer_id);
-        //$booking_status = $customer->bookingStatus()->findOrFail($request->booking_id);
+
         $booking_detail = $customer->bookingDetail()->findOrFail($request->booking_detail_id);
-        $booking_detail->update(["status" => 'suspend' ,'suspend_reason' => $request->suspension_reason,'suspend_started' => date('Y-m-d',strtotime($request->suspensionstartdate)),'suspend_ended' =>date('Y-m-d',strtotime($request->suspensionenddate)) ,'suspend_fee' => $request->suspension_fee,'suspend_comment' =>$request->suspension_comment]);
+
+        if($booking_detail->can_suspend()){
+            if($request->suspension_fee > 0){
+                if($customer->charge($request->suspension_fee, 'terminate')){
+                    $booking_detail->update(["status" => 'suspend' ,
+                                            'suspend_reason' => $request->suspension_reason,
+                                            'suspend_started' => date('Y-m-d',strtotime($request->suspensionstartdate)),
+                                            'suspend_ended' =>date('Y-m-d',strtotime($request->suspensionenddate)) ,
+                                            'suspend_fee' => $request->suspension_fee,
+                                            'suspend_comment' =>$request->suspension_comment]);
+                }else{
+                    return response()->json(['message' => 'charge user failed.'], 400);
+                }
+            }
+        }else{
+            return response()->json(['message' => 'is not acive membership, can not suspend.'], 400);
+        }
+        
     } 
 
     public function terminate(Request $request, $business_id){
@@ -133,11 +150,11 @@ class UserBookingDetailController extends Controller
         $customer = $company->customers()->findOrFail($request->customer_id);
 
         $booking_detail = $customer->bookingDetail()->findOrFail($request->booking_detail_id);
-        
+
         if($booking_detail->can_terminate()){
             if($request->terminate_fee > 0){
                 if($customer->charge($request->terminate_fee, 'terminate')){
-                    $booking_detail->update(["status" => 'cancel' ,
+                    $booking_detail->update(["status" => 'terminate' ,
                                             'expired_at' => date('Y-m-d',strtotime($request->terminated_at)),
                                             'terminate_reason' => $request->terminate_reason,
                                             'terminated_at' => date('Y-m-d',strtotime($request->terminated_at)),
@@ -156,12 +173,13 @@ class UserBookingDetailController extends Controller
         $company = $request->current_company->findOrFail($business_id);
 
         $customer = $company->customers()->findOrFail($request->customer_id);
-
+        
 
         $transaction = $customer->Transaction()->where('item_id', $booking_id)->first();
 
         if($transaction->can_void()){
             $transaction->void(); 
+            $customer->bookingDetail()->where('booking_id', $booking_id)->update(["status" => 'void']);
         }else{
             return response()->json(['message' => 'transction not found or already complete, can only refund'], 400);
         }
