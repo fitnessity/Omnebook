116cc16497223f4978640c19715c7b7821a684e4
odin <odin@1in.tw>
2023-12-28 17:23:30 -0500

    fix

--- resources/views/customers/show.blade.php
+++ resources/views/customers/show.blade.php
@@ -855,7 +855,10 @@
 																											<td>{{$history->item_description(request()->business_id)['qty']}}</td>
 																											<td>
 																												@if(($history->can_void() && $history->item_type=="UserBookingStatus") || ($history->can_refund()))
-																													<a href="#" data-behavior="ajax_html_modal" data-url="{{route('void_or_refund_modal', ['business_id' => request()->business_id, 'id' => $customerdata->id,'booking_detail_id' => 0 , 'booking_id' => $history->item_id])}}" data-modal-width="modal-100">Void</a>
+																													<?php
+																													$detail = $history->userBookingStatus->UserBookingDetail->first();
+																													?>
+																													<a href="#" data-behavior="ajax_html_modal" data-url="{{route('void_or_refund_modal', ['business_id' => request()->business_id, 'id' => $customerdata->id,'booking_detail_id' => $detail->id , 'booking_id' => $history->item_id])}}" data-modal-width="modal-100">Void</a>
 																												@else
 																													{{$history->status}}
 																												@endif
