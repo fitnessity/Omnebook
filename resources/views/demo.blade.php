8a2ba2f5a42c5bd53cf6e1dcb0711117d6a68144
Purvi Patel <purvikhoyani14@gmail.com>
2024-10-11 14:48:37 +0000

    files

--- resources/views/demo.blade.php
+++ resources/views/demo.blade.php
@@ -1,7 +1,5 @@
 <html>
-
 <body>
-    <h1>'test</h1>
-    <iframe src="http://vrajesh.govindcrankrod.com/" style="height:800x;width:800px;" title="Iframe Example"  sandbox="allow-same-origin allow-scripts"></iframe>
+<h4>test</h4>
 </body>
 </html> 
